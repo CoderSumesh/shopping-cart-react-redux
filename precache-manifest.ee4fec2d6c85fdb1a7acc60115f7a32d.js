83411fd5ac3cb329cdad6b2d81c50641be916546
Sumesh S <sumesh.yantramind@gmail.com>
2020-05-17 15:50:24 +0530

    Updates

---
+++ precache-manifest.ee4fec2d6c85fdb1a7acc60115f7a32d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c047c88365455c8b03cbbbcd45dade45",
+    "url": "/shopping-cart-assignment/index.html"
+  },
+  {
+    "revision": "74e403864bae208eb09b",
+    "url": "/shopping-cart-assignment/static/css/2.7c33a44d.chunk.css"
+  },
+  {
+    "revision": "e2745c3a0e77a25d060f",
+    "url": "/shopping-cart-assignment/static/css/main.7e55314f.chunk.css"
+  },
+  {
+    "revision": "74e403864bae208eb09b",
+    "url": "/shopping-cart-assignment/static/js/2.9ed2ad4e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/shopping-cart-assignment/static/js/2.9ed2ad4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2745c3a0e77a25d060f",
+    "url": "/shopping-cart-assignment/static/js/main.04ab5903.chunk.js"
+  },
+  {
+    "revision": "780b097eced00c8d967f",
+    "url": "/shopping-cart-assignment/static/js/runtime-main.fa17af27.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/shopping-cart-assignment/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/shopping-cart-assignment/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/shopping-cart-assignment/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/shopping-cart-assignment/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/shopping-cart-assignment/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
